5ee047d22e9d880520cbafd5e045f49fdc4796f9
Christian <carl_hassel@hotmail.com>
2020-10-23 15:45:08 +0200

    test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void importantStringTest() {
+        String s = "John";
+        Assertions.assertEquals("hitler", s);
     }
 
 }
